821a3f81a5a1ed53eb3c554a317926e0fc802795
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-11 16:05:36 +0100

    Update model.jl

--- Agents/Schelling/model.jl
+++ Agents/Schelling/model.jl
@@ -15,7 +15,7 @@ schelling(;
 ```
 Same as in [Schelling's segregation model](@ref).
 """
-function schelling(; numagents = 400, griddims = (50, 50), min_to_be_happy = 3)
+function schelling(; numagents = 2000, griddims = (50, 50), min_to_be_happy = 3)
     @assert numagents < prod(griddims)
     space = GridSpaceSingle(griddims, periodic = false)
     properties = Dict(:min_to_be_happy => min_to_be_happy)
