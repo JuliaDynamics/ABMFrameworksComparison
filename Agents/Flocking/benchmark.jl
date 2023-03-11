4640bcbfa6679f1ec5107db2974b5ca9c03e79df
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-11 13:47:36 +0100

    Update benchmark.jl

--- Agents/Flocking/benchmark.jl
+++ Agents/Flocking/benchmark.jl
@@ -4,8 +4,7 @@ using BenchmarkTools
 include("model.jl")
 
 a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
-    (model, agent_step!, model_step!) = flocking()
-)
+    (model, agent_step!, model_step!) = flocking()) samples=100
 
 println("Agents.jl Flocking (ms): ", minimum(a.times) * 1e-6)
 
