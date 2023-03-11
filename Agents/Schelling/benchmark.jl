94703bb5b2370cff83c3cd4eef5b60fb8a8c5303
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-11 14:03:26 +0100

    Update benchmark.jl

--- Agents/Schelling/benchmark.jl
+++ Agents/Schelling/benchmark.jl
@@ -3,7 +3,7 @@ using BenchmarkTools
 
 include("model.jl")
 
-a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
+a = @benchmark step!(model, agent_step!, model_step!, 10) setup = (
     (model, agent_step!, model_step!) = schelling()) samples = 100
 
 println("Agents.jl Schelling (ms): ", minimum(a.times) * 1e-6)
