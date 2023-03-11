f16c59447e4398d33f97cc12d5a7522dc1c85f70
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-11 14:03:57 +0100

    Update benchmark.jl

--- Agents/WolfSheep/benchmark.jl
+++ Agents/WolfSheep/benchmark.jl
@@ -3,7 +3,7 @@ using BenchmarkTools
 
 include("model.jl")
 
-a = @benchmark step!(model, agent_step!, model_step!, 100) setup = (
+a = @benchmark step!(model, agent_step!, model_step!, 500) setup = (
     (model, agent_step!, model_step!) = predator_prey()) samples = 100
 
 println("Agents.jl WolfSheep (ms): ", minimum(a.times) * 1e-6)
