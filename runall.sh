15c7c4f9a55e4a2952d31ecde632d81f5187506b
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-26 00:06:08 +0100

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
 echo "Benchmarking Julia"
-julia --project=@. Agents/WolfSheep/benchmark.jl
-julia --project=@. Agents/Flocking/benchmark.jl
-julia --project=@. Agents/Schelling/benchmark.jl
-julia --project=@. Agents/ForestFire/benchmark.jl
+julia --project=@. Agents/WolfSheep/benchmark_wolfsheep.jl
+julia --project=@. Agents/Flocking/benchmark_flocking.jl
+julia --project=@. Agents/Schelling/benchmark_schelling.jl
+julia --project=@. Agents/ForestFire/benchmark_forestfire.jl
 
 echo "Benchmarking Mason"
-bash Mason/Flocking/bench.sh
-bash Mason/Schelling/bench.sh
+bash Mason/Flocking/benchmark_flocking.sh
+bash Mason/Schelling/benchmark_schelling.sh
 
 echo "Benchmarking Mesa"
-python Mesa/WolfSheep/benchmark.py
-python Mesa/Flocking/benchmark.py
-python Mesa/Schelling/benchmark.py
-python Mesa/ForestFire/benchmark.py
+python Mesa/WolfSheep/benchmark_wolfsheep.py
+python Mesa/Flocking/benchmark_flocking.py
+python Mesa/Schelling/benchmark_schelling.py
+python Mesa/ForestFire/benchmark_forestfire.py
 
 echo "Benchmarking NetLogo"
-# Don't run above 8 threads otherwise errors will spit once the JVMs try
-# to share the Backing Store and lock it
-ws=$(parallel -j8 ::: $(printf './netlogo_ws.sh %.0s' {1..100}) | sort | head -n1)
+ws=$($(printf 'NetLogo/WolfSheep/benchmark_wolfsheep.sh %.0s' {1..100}) | sort | head -n1)
 echo "NetLogo WolfSheep (ms): "$ws
 
-ws=$(parallel -j8 ::: $(printf './netlogo_flock.sh %.0s' {1..100}) | sort | head -n1)
+ws=$($(printf 'NetLogo/Flocking/benchmark_flocking.sh %.0s' {1..100}) | sort | head -n1)
 echo "NetLogo Flocking (ms): "$ws
 
-ws=$(parallel -j8 ::: $(printf './netlogo_s.sh %.0s' {1..100}) | sort | head -n1)
+ws=$($(printf 'NetLogo/Schelling/benchmark_schelling.sh %.0s' {1..100}) | sort | head -n1)
 echo "NetLogo Schelling (ms): "$ws
 
-ws=$(parallel -j8 ::: $(printf './netlogo_forest.sh %.0s' {1..100}) | sort | head -n1)
+ws=$($(printf 'NetLogo/ForestFire/benchmark_forestfire.sh %.0s' {1..100}) | sort | head -n1)
 echo "NetLogo ForestFire (ms): "$ws
