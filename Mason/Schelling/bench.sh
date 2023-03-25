f9b92b35dbacd9ddf5863bed1fe799a85e6efc49
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-25 12:33:17 +0100

    Update bench.sh

--- Mason/Schelling/bench.sh
+++ Mason/Schelling/bench.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#export CLASSPATH mason.20.jar
+export CLASSPATH=Mason/Schelling:Mason/mason.21.jar
+javac Mason/Schelling/Schelling.java Mason/Schelling/Agent.java
 times=()
 for i in {1..100}
 do
     startt=`date +%s%N`
-    java sim.app.schelling.Schelling -for 10 -quiet
+    java Schelling -for 100 -quiet
     endt=`date +%s%N`
     times+=(`expr $endt - $startt`)
 done
 readarray -t sorted < <(printf '%s\n' "${times[@]}" | sort)
+echo -n "Mason Schelling (ms): "
 echo "${sorted[0]} * 0.000001" | bc
