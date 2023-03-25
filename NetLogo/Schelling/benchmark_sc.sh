e4562fe287a0fc7ea18e96ecdfbcd601bd24d650
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-25 23:46:21 +0100

    Update and rename netlogo_s.sh to NetLogo/Schelling/benchmark_sc.sh

--- NetLogo/Schelling/benchmark_sc.sh
+++ NetLogo/Schelling/benchmark_sc.sh
@@ -3,4 +3,4 @@
 # NetLogo's profiler sucks in the sense that it times one run, then spits out a bunch of junk
 # to either a file or stdout. There's no easy abilitiy to parse it.
 
-JAVA_HOME=/usr /opt/netlogo/netlogo-headless.sh --model "NetLogo/Schelling/Segregation.nlogo" --experiment benchmark | awk '/GO/{i++}i==2{print $3;exit}'
+bash path/to/netlogo/netlogo-headless.sh --model "Schelling.nlogo" --experiment benchmark | awk '/GO/{i++}i==2{print $3;exit}'
