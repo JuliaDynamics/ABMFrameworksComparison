9525a2e75ff8dedb613b754c9c7b870cb8417807
Tortar <68152031+Tortar@users.noreply.github.com>
2023-03-27 22:49:46 +0200

    Create create_benchmark_table.jl

---
+++ create_benchmark_table.jl
@@ -0,0 +1,35 @@
+using PrettyTables
+
+frameworks = ["Agents.jl", "Mason", "Mesa", "NetLogo"]
+models = ["WolfSheep", "Flocking", "Schelling", "ForestFire"]
+
+frameworks_times = Dict(m => Dict(f => 0.0 for f in frameworks) for m in models)
+		  
+open("benchmark_results.txt", "r") do f
+    for line in readlines(f)
+        s_line = split(line)
+        !in(s_line[1], frameworks) && continue
+        frameworks_times[String(s_line[2])][String(s_line[1])] = parse(Float64, s_line[4])
+    end
+end
+
+frameworks_comparison = Dict(m => Dict(f => 0.0 for f in frameworks) for m in models)
+for m in models
+    for f in frameworks
+        if f == "Agents.jl"
+            frameworks_comparison[m][f] = 1
+        else
+            if frameworks_times[m][f] == 0.0
+                continue
+            else
+                v = round(frameworks_times[m][f]/frameworks_times[m]["Agents.jl"], digits=1)
+                frameworks_comparison[m][f] = v
+            end
+        end
+    end
+end
+
+columns = ["Model/Framework", "Agents 5.12.0", "MASON 21.0", "Mesa 1.2.0", "Netlogo 6.4"]
+results = mapreduce(permutedims, vcat, [vcat([m], [ifelse(frameworks_comparison[m][f] != 0, frameworks_comparison[m][f], ".") for f in frameworks]) for m in models])
+conf = set_pt_conf(tf = tf_markdown, alignment = :c)
+table = pretty_table_with_conf(conf, results; header = columns)
