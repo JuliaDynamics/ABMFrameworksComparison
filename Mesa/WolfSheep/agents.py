8c3bd03f02e60392b949de13ad10d28f5e1b473a
Tortar <68152031+Tortar@users.noreply.github.com>
2023-04-01 00:23:28 +0200

    Update agents.py

--- Mesa/WolfSheep/agents.py
+++ Mesa/WolfSheep/agents.py
@@ -3,42 +3,30 @@ from random_walk import RandomWalker
 
 
 class Sheep(RandomWalker):
-    """
-    A sheep that walks around, reproduces (asexually) and gets eaten.
 
-    The init is the same as the RandomWalker.
-    """
-    
     def __init__(self, unique_id, pos, model, moore, energy=None):
         super().__init__(unique_id, pos, model, moore=moore)
         self.energy = energy
 
     def step(self):
-        """
-        A model step. Move, then eat grass and reproduce.
-        """
         self.random_move()
         living = True
 
         if self.model.grass:
-            # Reduce energy
             self.energy -= 1
 
-            # If there is grass available, eat it
             this_cell = self.model.grid.get_cell_list_contents(self.pos)
             grass_patch = next(obj for obj in this_cell if isinstance(obj, GrassPatch))
             if grass_patch.fully_grown:
                 self.energy += self.model.sheep_gain_from_food
                 grass_patch.fully_grown = False
 
-            # Death
             if self.energy < 0:
                 self.model.grid.remove_agent(self)
                 self.model.schedule.remove(self)
                 living = False
 
         if living and self.random.random() < self.model.sheep_reproduce:
-            # Create a new sheep:
             if self.model.grass:
                 self.energy /= 2
             lamb = Sheep(
@@ -49,9 +37,6 @@ class Sheep(RandomWalker):
 
 
 class Wolf(RandomWalker):
-    """
-    A wolf that walks around, reproduces (asexually) and eats sheep.
-    """
 
     energy = None
 
@@ -63,7 +48,6 @@ class Wolf(RandomWalker):
         self.random_move()
         self.energy -= 1
 
-        # If there are sheep present, eat one
         x, y = self.pos
         this_cell = self.model.grid.get_cell_list_contents([self.pos])
         sheep = [obj for obj in this_cell if isinstance(obj, Sheep)]
@@ -71,17 +55,14 @@ class Wolf(RandomWalker):
             sheep_to_eat = self.random.choice(sheep)
             self.energy += self.model.wolf_gain_from_food
 
-            # Kill the sheep
             self.model.grid.remove_agent(sheep_to_eat)
             self.model.schedule.remove(sheep_to_eat)
 
-        # Death or reproduction
         if self.energy < 0:
             self.model.grid.remove_agent(self)
             self.model.schedule.remove(self)
         else:
             if self.random.random() < self.model.wolf_reproduce:
-                # Create a new wolf cub
                 self.energy /= 2
                 cub = Wolf(
                     self.model.next_id(), self.pos, self.model, self.moore, self.energy
@@ -91,18 +72,8 @@ class Wolf(RandomWalker):
 
 
 class GrassPatch(Agent):
-    """
-    A patch of grass that grows at a fixed rate and it is eaten by sheep
-    """
 
     def __init__(self, unique_id, pos, model, fully_grown, countdown):
-        """
-        Creates a new patch of grass
-
-        Args:
-            grown: (boolean) Whether the patch of grass is fully grown or not
-            countdown: Time for the patch of grass to be fully grown again
-        """
         super().__init__(unique_id, model)
         self.fully_grown = fully_grown
         self.countdown = countdown
@@ -111,7 +82,6 @@ class GrassPatch(Agent):
     def step(self):
         if not self.fully_grown:
             if self.countdown <= 0:
-                # Set as fully grown
                 self.fully_grown = True
                 self.countdown = self.model.grass_regrowth_time
             else:
