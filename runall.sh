2b846f0c956c02a252e430c9003da21b554cb633
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-11 19:41:59 +0200

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 (
+echo "Benchmarking Julia"
+julia --project=@. WolfSheep/Agents/benchmark_wolfsheep.jl
+julia --project=@. Flocking/Agents/benchmark_flocking.jl
+julia --project=@. Schelling/Agents/benchmark_schelling.jl
+julia --project=@. ForestFire/Agents/benchmark_forestfire.jl
+
+echo "Benchmarking Mason"
+bash Flocking/Mason/benchmark_flocking.sh
+bash Schelling/Mason/benchmark_schelling.sh
+
+echo "Benchmarking Mesa"
+python3 WolfSheep/Mesa/benchmark_wolfsheep.py
+python3 Flocking/Mesa/benchmark_flocking.py
+python3 Schelling/Mesa/benchmark_schelling.py
+python3 ForestFire/Mesa/benchmark_forestfire.py
+
 echo "Benchmarking NetLogo"
 # Don't run above 8 threads otherwise errors will spit once the JVMs try
 # to share the Backing Store and lock it
@@ -14,6 +30,7 @@ ws=$(parallel -j1 ::: $(printf 'Schelling/NetLogo/benchmark_schelling.sh %.0s' {
 echo "NetLogo Schelling (ms): "$ws
 
 ws=$(parallel -j1 ::: $(printf 'ForestFire/NetLogo/benchmark_forestfire.sh %.0s' {1..100}) | sort | head -n1)
-echo "NetLogo ForestFire (ms): "$ws) | tee benchmark_results.txt
+echo "NetLogo ForestFire (ms): "$ws
+) | tee benchmark_results.txt
 
 julia create_benchmark_table.jl
