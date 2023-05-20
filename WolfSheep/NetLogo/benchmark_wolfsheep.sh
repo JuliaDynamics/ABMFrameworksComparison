e6a02f2822f0e8ad922b90df84ebc5913fd4b87d
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-20 15:15:35 +0200

    Update benchmark_wolfsheep.sh

--- WolfSheep/NetLogo/benchmark_wolfsheep.sh
+++ WolfSheep/NetLogo/benchmark_wolfsheep.sh
@@ -4,4 +4,18 @@
 # to either a file or stdout. There's no easy abilitiy to parse it.
 
 # the netlogo folder is assumed to be inside the NetLogo folder of this repository, which contains the models
-bash ./netlogo/netlogo-headless.sh --model "WolfSheep/NetLogo/WolfSheep.nlogo" --setup-file "WolfSheep/NetLogo/parameters_wolfsheep.xml" --experiment benchmark | awk '/GO/{i++}i==2{print $3;exit}'
+
+NAME_LAUNCHER="./netlogo/netlogo-headless.sh"
+NAME_MODEL="WolfSheep/NetLogo/WolfSheep.nlogo"
+NAME_PARAM="WolfSheep/NetLogo/parameters_wolfsheep.xml"
+
+times=()
+for i in {1..100}
+do
+    t=$((bash $NAME_LAUNCHER --model $NAME_MODEL --setup-file $NAME_PARAM --experiment benchmark
+    	) | awk '/GO/{i++}i==2{print $3;exit}')
+    times+=(`expr $t`)
+done
+
+readarray -t sorted < <(printf '%s\n' "${times[@]}" | sort)
+printf "NetLogo Schelling (ms): "${sorted[0]}"\n"
