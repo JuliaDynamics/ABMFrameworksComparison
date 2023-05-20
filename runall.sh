65479d1082cc2c7a9f85a589551dc73ee6466dba
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-20 15:16:24 +0200

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -18,10 +18,7 @@ python3 Schelling/Mesa/benchmark_schelling.py
 python3 ForestFire/Mesa/benchmark_forestfire.py
 
 echo "Benchmarking NetLogo"
-# Don't run above 8 threads otherwise errors will spit once the JVMs try
-# to share the Backing Store and lock it
-ws=$(parallel -j1 ::: $(printf 'WolfSheep/NetLogo/benchmark_wolfsheep.sh %.0s' {1..100}) | sort | head -n1)
-echo "NetLogo WolfSheep (ms): "$ws
+bash 'WolfSheep/NetLogo/benchmark_wolfsheep.sh'
 
 ws=$(parallel -j1 ::: $(printf 'Flocking/NetLogo/benchmark_flocking.sh %.0s' {1..100}) | sort | head -n1)
 echo "NetLogo Flocking (ms): "$ws
