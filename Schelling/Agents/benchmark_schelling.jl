afc3414747ff37b8e7550d3e60b549c92136b149
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-24 23:20:26 +0200

    Force BenchmarkTools to run for all samples (#26)

--- Schelling/Agents/benchmark_schelling.jl
+++ Schelling/Agents/benchmark_schelling.jl
@@ -8,7 +8,7 @@ rng_seed = MersenneTwister(42)
 
 n_run = 100
 a = @benchmark step!(model, agent_step!, model_step!, 10) setup = 
-    ((model, agent_step!, model_step!) = schelling(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run
+    ((model, agent_step!, model_step!) = schelling(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run seconds=1e6
 
 median_time = sort(a.times)[n_run ÷ 2 + n_run % 2]
 println("Agents.jl Schelling (ms): ", minimum(a.times) * 1e-6)
--- WolfSheep/Agents/benchmark_wolfsheep.jl
+++ WolfSheep/Agents/benchmark_wolfsheep.jl
@@ -8,7 +8,7 @@ rng_seed = MersenneTwister(42)
 
 n_run = 100
 a = @benchmark step!(model, agent_step!, model_step!, 500) setup = 
-    ((model, agent_step!, model_step!) = predator_prey(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run 
+    ((model, agent_step!, model_step!) = predator_prey(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run seconds=1e6
 
 median_time = sort(a.times)[n_run ÷ 2 + n_run % 2]
 println("Agents.jl WolfSheep (ms): ", median_time * 1e-6)
