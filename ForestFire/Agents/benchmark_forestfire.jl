2837153e8e6c5d10e1b303212b4f9d61e4100d4c
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-24 23:18:39 +0200

    Update benchmark_forestfire.jl

--- ForestFire/Agents/benchmark_forestfire.jl
+++ ForestFire/Agents/benchmark_forestfire.jl
@@ -8,7 +8,7 @@ rng_seed = MersenneTwister(42)
 
 n_run = 100
 a = @benchmark step!(model, agent_step!, model_step!, 100) setup =
-    ((model, agent_step!, model_step!) = forest_fire(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run
+    ((model, agent_step!, model_step!) = forest_fire(Xoshiro(rand(rng_seed, 1:10000)))) evals=1 samples=n_run seconds=1e6
 
 median_time = sort(a.times)[n_run ÷ 2 + n_run % 2]
 println("Agents.jl ForestFire (ms): ", median_time * 1e-6)
