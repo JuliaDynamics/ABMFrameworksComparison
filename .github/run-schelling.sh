3838a5358d7ac5ddcb1e450b331215794d4129fd
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-28 16:02:53 +0200

    Update run-schelling.sh

--- .github/run-schelling.sh
+++ .github/run-schelling.sh
@@ -3,10 +3,10 @@
 (
 
 echo "Benchmarking Agents.jl"
-julia --project=@. ../Schelling/Agents/benchmark_schelling.jl
+julia --project=@. ./Schelling/Agents/benchmark_schelling.jl
 
 echo "Benchmarking Mesa"
-python3 ../Schelling/Mesa/benchmark_schelling.py
+python3 ./Schelling/Mesa/benchmark_schelling.py
 
 ) | tee benchmark_results.txt
 
