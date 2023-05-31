d93b75d53243fc6e1778f0e42edf6ac33e80a147
Tortar <68152031+Tortar@users.noreply.github.com>
2023-05-31 18:46:09 +0200

    Add ForestFire model for MASON (#32)

--- .github/run-forestfire.sh
+++ .github/run-forestfire.sh
@@ -5,11 +5,14 @@
 echo "Benchmarking Agents.jl"
 julia --project=@. ./ForestFire/Agents/benchmark_forestfire.jl
 
+echo "Benchmarking Mason"
+bash ./ForestFire/Mason/benchmark_forestfire.sh
+
 echo "Benchmarking Mesa"
 python3 ./ForestFire/Mesa/benchmark_forestfire.py
 
 echo "Benchmarking NetLogo"
-bash ForestFire/NetLogo/benchmark_forestfire.sh
+bash ./ForestFire/NetLogo/benchmark_forestfire.sh
 
 ) | tee benchmark_results.txt
 
--- runall.sh
+++ runall.sh
@@ -2,7 +2,7 @@
 
 (
 
-echo "Benchmarking Julia"
+echo "Benchmarking Agents.jl"
 julia --project=@. WolfSheep/Agents/benchmark_wolfsheep.jl
 julia --project=@. Flocking/Agents/benchmark_flocking.jl
 julia --project=@. Schelling/Agents/benchmark_schelling.jl
@@ -11,6 +11,7 @@ julia --project=@. ForestFire/Agents/benchmark_forestfire.jl
 echo "Benchmarking Mason"
 bash Flocking/Mason/benchmark_flocking.sh
 bash Schelling/Mason/benchmark_schelling.sh
+bash ForestFire/Mason/benchmark_forestfire.sh
 
 echo "Benchmarking Mesa"
 python3 WolfSheep/Mesa/benchmark_wolfsheep.py
