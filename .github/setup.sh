160b103ba3ddf4aa6c0d7d149abbafbe336311e0
Tortar <68152031+Tortar@users.noreply.github.com>
2023-07-17 22:56:11 +0200

    Update setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -10,17 +10,17 @@ sudo apt install default-jre-headless
 sudo apt install default-jdk-headless
 
 # install julia
-sudo wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.0-linux-x86_64.tar.gz
-sudo tar zxvf julia-1.9.0-linux-x86_64.tar.gz
-export PATH=$PATH:$(pwd)"/julia-1.9.0/bin"
-printf "\nexport PATH=\"\$PATH:"$(pwd)"/julia-1.9.0/bin\"" >> ~/.bashrc
+sudo wget https://julialang-s3.julialang.org/bin/linux/x64/1.9/julia-1.9.2-linux-x86_64.tar.gz
+sudo tar zxvf julia-1.9.2-linux-x86_64.tar.gz
+export PATH=$PATH:$(pwd)"/julia-1.9.2/bin"
+printf "\nexport PATH=\"\$PATH:"$(pwd)"/julia-1.9.2/bin\"" >> ~/.bashrc
 
 # install agents
 julia --project=@. -e 'using Pkg; Pkg.instantiate()'
 
 # install mesa
 sudo apt install python3-pip
-pip install mesa==1.2.1
+pip install mesa==2.0.0
 
 # install netlogo
 sudo wget http://ccl.northwestern.edu/netlogo/6.3.0/NetLogo-6.3.0-64.tgz
