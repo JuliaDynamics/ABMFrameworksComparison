752247443bda35ebaa834abc0815d83da0cbbbc6
Tortar <68152031+Tortar@users.noreply.github.com>
2023-08-04 21:42:09 +0200

    Update Agent_large.java

--- Schelling/Mason/Agent_large.java
+++ Schelling/Mason/Agent_large.java
@@ -1,4 +1,4 @@
-import sim.field.grid.IntGrid2D;
+import sim.field.grid.ObjectGrid2D;
 import sim.engine.SimState;
 import sim.util.IntBag;
 import sim.util.Int2D;
@@ -7,53 +7,49 @@ import sim.engine.Steppable;
 public class Agent_large implements Steppable
 {
     Int2D loc;
+    public ObjectGrid2D neighbors;
+    public int group;
+    public boolean mood;
     IntBag neighborsX;
     IntBag neighborsY;
 
-    public Agent_large(final int x, final int y) {
+    public Agent_large(final int x, final int y, final int group, final boolean mood) {
+        this.loc = new Int2D(x, y);
+        this.group = group;
+        this.mood = mood;
         this.neighborsX = new IntBag(25);
         this.neighborsY = new IntBag(25);
-        this.loc = new Int2D(x, y);
     }
 
     public void step(final SimState state) {
         final Schelling_large sch = (Schelling_large)state;
-        final int[][] locs = sch.neighbors.field;
         final int x = this.loc.x;
         final int y = this.loc.y;
-        if (locs[x][y] < 2) {
-            return;
-        }
-        if (sch.emptySpaces.numObjs == 0) {
-            return;
-        }
-        final IntGrid2D neighbors = sch.neighbors;
-        final int x2 = this.loc.x;
-        final int y2 = this.loc.y;
         final int neighborhood = sch.neighborhood;
-        final IntGrid2D neighbors2 = sch.neighbors;
-        neighbors.getMooreLocations(x2, y2, neighborhood, 0, true, this.neighborsX, this.neighborsY);
+        final ObjectGrid2D neighbors2 = sch.neighbors;
+        neighbors2.getMooreLocations(x, y, neighborhood, 0, true, this.neighborsX, this.neighborsY);
         double val = 0.0;
         final int threshold = sch.threshold;
         final int numObjs = this.neighborsX.numObjs;
         final int[] objsX = this.neighborsX.objs;
         final int[] objsY = this.neighborsY.objs;
-        final int myVal = locs[x][y];
         for (int i = 0; i < numObjs; ++i) {
-            if (locs[objsX[i]][objsY[i]] == myVal && (objsX[i] != x || objsY[i] != y)) {
-                val += 1.0;
-                if (val >= threshold) {
-                    return;
+            Object o = neighbors2.field[objsX[i]][objsY[i]];
+            if (o != null) {
+                Agent_large neigh = (Agent_large)o;
+                if (neigh.group == this.group && (objsX[i] != x || objsY[i] != y)) {
+                    val += 1.0;
                 }
             }
         }
-        final int newLocIndex = state.random.nextInt(sch.emptySpaces.numObjs);
-        final Int2D newLoc = (Int2D)sch.emptySpaces.objs[newLocIndex];
-        sch.emptySpaces.objs[newLocIndex] = this.loc;
-        final int swap = locs[newLoc.x][newLoc.y];
-        locs[newLoc.x][newLoc.y] = locs[this.loc.x][this.loc.y];
-        locs[this.loc.x][this.loc.y] = swap;
-        this.loc = newLoc;
+        if (val >= threshold) {
+            this.mood = true;     
+            }
+        else {
+            final int newLocIndex = state.random.nextInt(sch.emptySpaces.numObjs);
+            final Int2D newLoc = (Int2D)sch.emptySpaces.objs[newLocIndex];
+            sch.emptySpaces.objs[newLocIndex] = this.loc;
+            this.loc = newLoc;
+        }
     }
 }
-
