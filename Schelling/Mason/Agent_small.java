bf42bb3c7ed926691e3e96d2c2f35ec143edf1ad
Tortar <68152031+Tortar@users.noreply.github.com>
2023-08-04 21:22:02 +0200

    Update Agent_small.java

--- Schelling/Mason/Agent_small.java
+++ Schelling/Mason/Agent_small.java
@@ -8,12 +8,12 @@ public class Agent_small implements Steppable
 {
     Int2D loc;
     public ObjectGrid2D neighbors;
-    public int group
-    public bool mood
+    public int group;
+    public boolean mood;
     IntBag neighborsX;
     IntBag neighborsY;
 
-    public Agent_small(final int x, final int y, final int group, final bool mood) {
+    public Agent_small(final int x, final int y, final int group, final boolean mood) {
         this.loc = new Int2D(x, y);
         this.group = group;
         this.mood = mood;
@@ -23,32 +23,33 @@ public class Agent_small implements Steppable
 
     public void step(final SimState state) {
         final Schelling_small sch = (Schelling_small)state;
-        final int[][] locs = sch.neighbors.field;
         final int x = this.loc.x;
         final int y = this.loc.y;
         final int neighborhood = sch.neighborhood;
-        final IntGrid2D neighbors2 = sch.neighbors;
-        neighbors.getMooreLocations(x, y, neighborhood, 0, true, this.neighborsX, this.neighborsY);
+        final ObjectGrid2D neighbors2 = sch.neighbors;
+        neighbors2.getMooreLocations(x, y, neighborhood, 0, true, this.neighborsX, this.neighborsY);
         double val = 0.0;
         final int threshold = sch.threshold;
         final int numObjs = this.neighborsX.numObjs;
         final int[] objsX = this.neighborsX.objs;
         final int[] objsY = this.neighborsY.objs;
-        final int myVal = locs[x][y];
         for (int i = 0; i < numObjs; ++i) {
-            if (locs[objsX[i]][objsY[i]] == myVal && (objsX[i] != x || objsY[i] != y)) {
-                val += 1.0;
+            Object o = neighbors2.field[objsX[i]][objsY[i]];
+            if (o != null) {
+                Agent_small neigh = (Agent_small)o;
+                if (neigh.group == this.group && (objsX[i] != x || objsY[i] != y)) {
+                    val += 1.0;
+                }
             }
+        }
+        
         if (val >= threshold) {
-            this.mood = true      
+            this.mood = true;     
             }
         else {
             final int newLocIndex = state.random.nextInt(sch.emptySpaces.numObjs);
             final Int2D newLoc = (Int2D)sch.emptySpaces.objs[newLocIndex];
             sch.emptySpaces.objs[newLocIndex] = this.loc;
-            final int swap = locs[newLoc.x][newLoc.y];
-            locs[newLoc.x][newLoc.y] = locs[this.loc.x][this.loc.y];
-            locs[this.loc.x][this.loc.y] = swap;
             this.loc = newLoc;
         }
     }
