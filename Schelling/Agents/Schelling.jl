8bfa2b6bebc8bfb614150b0561afb016ba98ac7a
Tortar <68152031+Tortar@users.noreply.github.com>
2023-11-15 13:13:40 +0100

    Update Schelling.jl

--- Schelling/Agents/Schelling.jl
+++ Schelling/Agents/Schelling.jl
@@ -8,15 +8,15 @@ end
 function schelling_model(rng, numagents, griddims, min_to_be_happy, radius)
     space = GridSpaceSingle(griddims, periodic = false)
     properties = (min_to_be_happy = min_to_be_happy, radius = radius)
-    model = UnremovableABM(SchellingAgent, space; agent_step! = schelling_agent_step!
-                           properties, scheduler = Schedulers.Randomly(), rng = rng)
+    model = StandardABM(SchellingAgent, space; agent_step!, properties, rng,
+                        container = Vector, scheduler = Schedulers.Randomly())
     for n in 1:numagents
         add_agent_single!(model, false, n < numagents / 2 ? 1 : 2)
     end
     return model
 end
 
-function schelling_agent_step!(agent, model)
+function agent_step!(agent, model)
     count_neighbors_same_group = 0
     for neighbor in nearby_agents(agent, model, model.radius)
         if agent.group == neighbor.group
